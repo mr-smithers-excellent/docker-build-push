f990371036b73d1cf955ab5f9c121803d08bc970
mr-smithers-excellent <seanpsmith85@gmail.com>
2019-10-17 21:38:05 -0700

    add test throwing error

--- tests/docker-build-push.test.js
+++ tests/docker-build-push.test.js
@@ -4,29 +4,46 @@ const core = require('@actions/core');
 const docker = require('../src/docker');
 const run = require('../src/docker-build-push');
 
+beforeAll(() => {
+  docker.build = jest.fn();
+  docker.push = jest.fn();
+});
+
 describe('Create & push Docker image', () => {
-  test('', async () => {
+  test('Valid Docker inputs', () => {
     const image = 'gcp-project/image';
-    const registry = 'https://gcr.io';
+    const registry = 'gcr.io';
     const tag = 'dev-1234567';
 
+    docker.login = jest.fn();
+    docker.createTag = jest.fn().mockReturnValueOnce(tag);
     core.getInput = jest
       .fn()
       .mockReturnValueOnce(image)
       .mockReturnValueOnce(registry)
       .mockReturnValueOnce(null);
-
     core.setOutput = jest.fn().mockReturnValueOnce(`${registry}/${image}:${tag}`);
 
-    docker.createTag = jest.fn().mockReturnValueOnce(tag);
-    docker.build = jest.fn();
-    docker.push = jest.fn();
-    docker.login = jest.fn();
-
-    await run();
+    run();
 
     expect(docker.createTag).toHaveBeenCalledTimes(1);
     expect(core.getInput).toHaveBeenCalledTimes(3);
     expect(core.setOutput).toHaveBeenCalledWith(`${registry}/${image}:${tag}`);
+  });
+});
+
+describe('Create Docker image causing an error', () => {
+  test('Docker login error', () => {
+    docker.createTag = jest.fn().mockRejectedValue('some-tag');
+    const error = 'Error: Cannot perform an interactive login from a non TTY device';
+    docker.login = jest.fn().mockImplementation(() => {
+      throw new Error(error);
+    });
+    core.setFailed = jest.fn();
+
+    run();
+
+    expect(docker.createTag).toHaveBeenCalledTimes(1);
+    expect(core.setFailed).toHaveBeenCalledWith(error);
   });
 });
