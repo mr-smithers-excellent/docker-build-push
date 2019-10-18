70fced1d3c1208db083f1399008bd1aa304e8e67
mr-smithers-excellent <seanpsmith85@gmail.com>
2019-10-18 12:29:06 -0700

    make username & password optional

--- dist/index.js
+++ dist/index.js
@@ -538,7 +538,7 @@ const isMasterBranch = ref => ref && ref === 'refs/heads/master';
 const isNotMasterBranch = ref => ref && ref.includes('refs/heads/') && ref !== 'refs/heads/master';
 
 const createTag = () => {
-  core.debug('Creating Docker image tag...');
+  core.info('Creating Docker image tag...');
   const { ref, sha } = context;
   const shortSha = sha.substring(0, 7);
   let dockerTag;
@@ -564,7 +564,7 @@ const createTag = () => {
 };
 
 const build = imageName => {
-  core.debug(`Building Docker image: ${imageName}...`);
+  core.info(`Building Docker image: ${imageName}...`);
   if (!fs.existsSync('./Dockerfile')) {
     core.setFailed('Dockerfile does not exist in root project directory!');
   }
@@ -572,19 +572,34 @@ const build = imageName => {
   cp.execSync(`docker build -t ${imageName} .`);
 };
 
-const login = () => {
-  core.debug('Logging into Docker registry...');
-  const registry = core.getInput('registry', { required: true });
-  const username = core.getInput('username', { required: true });
-  const password = core.getInput('password', { required: true });
+const isEcr = registry => {
+  return registry && registry.includes('amazonaws');
+};
 
-  cp.execSync(`docker login -u ${username} --password-stdin ${registry}`, {
-    input: password
-  });
+const getRegion = registry => {
+  return registry.substring(registry.indexOf('ecr.') + 4, registry.indexOf('.amazonaws'));
+};
+
+const login = () => {
+
+  const registry = core.getInput('registry', { required: true });
+  const username = core.getInput('username');
+  const password = core.getInput('password');
+
+  // If using ECR, use the AWS CLI login command in favor of docker login
+  if (isEcr(registry)) {
+    core.info('Logging into ECR...');
+    cp.execSync(`aws ecr get-login --region ${getRegion(registry)} --no-include-email`);
+  } else if (username && password) {
+    core.info('Logging into Docker registry...');
+    cp.execSync(`docker login -u ${username} --password-stdin ${registry}`, {
+      input: password
+    });
+  }
 };
 
 const push = imageName => {
-  core.debug('Pushing Docker image...');
+  core.info('Pushing Docker image...');
   cp.execSync(`docker push ${imageName}`);
 };
 
